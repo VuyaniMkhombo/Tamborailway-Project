fda75944379fea2b753dc019515619185b7ab615
VuyaniMkhombo <vuyanimkhombo@gmail.com>
2018-10-02 12:27:08 +0200

    changes

--- Day 6/script.js
+++ Day 6/script.js
@@ -1,69 +1,28 @@
-window.onload = function () {
-    var submitButton = document.getElementById('submit')
-    
-    submitButton.addEventListener('click', function(event) {
-        event.preventDefault()
-        
-        validateName()
-        validateSurname()
-        validateAge()
-    })
 
+function login(){
+    // create a client here: https://developer.whereismytransport.com/clients
+var CLIENT_ID = 'your client id';
+var CLIENT_SECRET = 'your client secret';
+var payload = {
+  'client_id': CLIENT_ID,
+  'client_secret': CLIENT_SECRET,
+  'grant_type': 'client_credentials',
+  'scope': 'transportapi:all'
+};
+var request = new XMLHttpRequest();
+request.open('POST', 'https://identity.whereismytransport.com/connect/token', true);
+request.addEventListener('load', function () {
+  var response = JSON.parse(this.responseText);
+  var token = response.access_token;
+  window.token = token;
+});
+request.setRequestHeader('Accept', 'application/json');
+var formData = new FormData();
+
+for (var key in payload) {
+  formData.append(key, payload[key]);
 }
 
-/**
- * This function validates the name 
- * from the name input
- */
-function validateName() {
-    var name = document.getElementById('name')
-    var nameErrorMessage = document.getElementById('name-error')
-    if(name.value == "" || name.value == undefined)
-    {
-        nameErrorMessage.classList.remove('is-invisible')
-    }
-    else {   
-        nameErrorMessage.classList.add('is-invisible')
-    }
+request.send(formData);
 }
-
-/**
- * This function validates the name 
- * from the name input
- */
-function validateSurname() {
-    var surname = document.getElementById('surname').value
-    var surnameErrorMessage = document.getElementById('surname-error')
-    if(surname == "" || surname == undefined)
-    {
-        surnameErrorMessage.classList.remove('is-invisible')
-    }
-    else {   
-        surnameErrorMessage.classList.add('is-invisible')
-    }
-}
-
-/**
- * This function validates the name 
- * from the name input
- */
-function validateAge() {
-    var age = document.getElementById('age').value
-    var tooYoungError = document.getElementById('age-error-young')
-    var tooOldError = document.getElementById('age-error-old')
-    
-    if(age < 18)
-    {
-        tooYoungError.classList.remove('is-invisible')
-        tooOldError.classList.add('is-invisible')
-    }
-    else if(age > 64) {
-        
-        tooOldError.classList.remove('is-invisible')
-        tooYoungError.classList.add('is-invisible')
-    }
-    else {   
-        tooOldError.classList.add('is-invisible')
-        tooYoungError.classList.add('is-invisible')
-    }
-}
+login()
